c428e0c4aee7f0f691890defa9bbbad0edb5b4e8
Rajat Dhir <rajat@infraguard.io>
2020-05-03 23:49:35 +0530

    serverless code complete

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
 const env = require('./loadenv').getEnvVar();
 const express = require('express') 
-const sls = require('serverless-http')
+const sls = require('serverless-http');
 const app = express();
 const serverFunction = require('./services/servers/serverFunction');
 
-app.get('/', async (req, res, next) => {	
+module.exports.server = (event, context, callback) => {
+    (async function () {
+		try {	
+			//await app.listen(env.PORT);
+			let response = await serverFunction.syncActiveClusters( (event.hasOwnProperty('ID'))?event.ID:'' ); 
+			callback(response.data);
+		} catch(error) { 
+			 callback(error);			
+		}
+	})(); 
+};
+
+/*app.get('/', async (req, res, next) => {	
 	(async function () {
 		try {			
 			console.log("==",env);
@@ -19,25 +31,6 @@ app.get('/', async (req, res, next) => {
 		} catch(error) { console.log("====error==",error);
 			 res.status(200).send({status:true,data:'',error:error})
 		}
-	})(); 	
-  
+	})();
 });
-
-app.get('/:ID', async (req, res, next) => {	
-	(async function () {
-		try {			
-			console.log("==",env);
-			//await app.listen(env.PORT);		
-
-			let response = await serverFunction.syncActiveClusters(req.params.ID); 
-			console.log("==response====",response);
-			 res.status(200).send({status:true,data:response.data,error:''})
-
-		} catch(error) { console.log("====error==",error);
-			 res.status(200).send({status:true,data:'',error:error})
-		}
-	})(); 	
-  
-})
-
-module.exports.server = sls(app)
+module.exports.server = sls(app)*/
