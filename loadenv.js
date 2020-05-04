106f878ff65d0ddc311a47f5573513a9d6a226ea
Rajat Dhir <rajat@infraguard.io>
2020-05-04 12:56:01 +0530

    sync platform servers

--- loadenv.js
+++ loadenv.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 function getEnvironmentVar() {
 	let envymlFileContent = fs.readFileSync('./env.yml', 'utf8');
     let fileContent = yaml.safeLoadAll(envymlFileContent);
-    if(fileContent[0][process.env.PRODUCTION_STAGE]){
-        return fileContent[0][process.env.PRODUCTION_STAGE]
+    if(fileContent[0][process.env.APP_STAGE]){
+        return fileContent[0][process.env.APP_STAGE]
     }	
 }
 module.exports.getEnvVar = getEnvironmentVar;
